9f05de38c2c0260a261a5e421df502665d817aba
Misha O'Keeffe <meokeeffe@wisc.edu>
2024-08-02 10:15:54 -0700

    changes to the credits

--- src/components/applications/Credits.tsx
+++ src/components/applications/Credits.tsx
@@ -8,22 +8,14 @@ export interface CreditsProps extends WindowAppProps {}
 const CREDITS = [
     {
         title: 'Engineering & Design',
-        rows: [['Henry Heffernan', 'All']],
+        rows: [['Henry Heffernan'],
+               [ 'Misha OKeeffe'],
+    ],
     },
     {
         title: 'Modeling & Texturing',
         rows: [
             ['Henry Heffernan', 'Texturing, Composition, & UV'],
-            ['Mickael Boitte', 'Computer Model'],
-            ['Sean Nicolas', 'Environment Models'],
-        ],
-    },
-    {
-        title: 'Sound Design',
-        rows: [
-            ['Henry Heffernan', 'Mixing, Composition, & Foley'],
-            ['Sound Cassette', 'Office Ambience'],
-            ['Windows 95 Startup Sound', 'Microsoft'],
         ],
     },
 ];
