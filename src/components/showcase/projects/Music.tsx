f89bd3da74bb81c4164ff4cfadd29cb63e19adff
Misha O'Keeffe <meokeeffe@wisc.edu>
2024-08-02 09:56:52 -0700

    open science

--- src/components/showcase/projects/Music.tsx
+++ src/components/showcase/projects/Music.tsx
@@ -1,224 +1,50 @@
-import React, { useState } from 'react';
-// @ts-ignore
-import house from '../../../assets/audio/house_master.mp3';
-// @ts-ignore
-import edge from '../../../assets/audio/edge_unmastered.mp3';
-// @ts-ignore
-import dnb from '../../../assets/audio/break.mp3';
-// @ts-ignore
-import dnbDrums from '../../../assets/audio/dnb_drop_drums.mp3';
-import houseProject from '../../../assets/pictures/projects/audio/houseProject.png';
-import dnbDrumsProject from '../../../assets/pictures/projects/audio/dnbDrumsProject.png';
-import { MusicPlayer } from '../../general';
+import React from 'react';
 
-export interface MusicProjectsProps {}
+export interface OpenScienceResourcesProps {}
 
-const MusicProjects: React.FC<MusicProjectsProps> = (props) => {
-    const [currentSong, setCurrentSong] = useState<string>('');
-
+const OpenScienceResources: React.FC<OpenScienceResourcesProps> = () => {
     return (
         <div className="site-page-content">
-            <h1>Music & Sound</h1>
-            <h3>Ventures</h3>
+            <h1>Open Science</h1>
+            <h3>Resources I Find Helpful</h3>
             <br />
             <div className="text-block">
                 <p>
-                    Music has been a passion of mine for my whole life. When I
-                    was 15 years old, I downloaded FL Studio and started to
-                    produce my own music. I found a wonderful community on
-                    reddit called r/edmproduction and in 2016 created the now
-                    popular discord server EDM Production with over 14k members.
+                Inspired by my former mentor, Rosie Aboody, I'm dedicated to promoting open science practices. 
+                Below you can find some resources (either of my own or others) that I hope will be of help. 
+                As I've found, inequitable access to resources is a huge barrier in academia; I hope this is at 
+                least a step in the right direction toward dismantling these systemic barriers. 
                 </p>
                 <br />
-                <p>
-                    I don't release the music that I make publicly as I not only
-                    find it difficult to finish songs, but also it's not a
-                    priority of mine since I produce mostly because I enjoy the
-                    process of learning and creating. However if I am going to
-                    dedicate a whole page of my portfolio to my music, you bet
-                    I'll have some examples to showoff.
-                </p>
-                <br />
-                <p>
-                    Below are some more recent Musical Endeavors I've been
-                    working on. I hope you enjoy the music and enjoy the stories
-                    behind each of the tracks.
-                </p>
             </div>
-            <h2>Exploring house</h2>
-            <br />
-            <p>
-                In 2020 I went through a phase of really enjoying house. Some of
-                my favorite artists at the time were Malaa, Chirs Lake, Kyle
-                Walker and many more. Inspired, I decided to take a stab at
-                making some house tracks.
-            </p>
-            <br />
-            <p>
-                I failed a few times as I wasn't really able to nail the style
-                and groove I was going for but ultimately ended up creating the
-                piece below.
-            </p>
-            <br />
 
-            <MusicPlayer
-                src={house}
-                title="Timeless"
-                subtitle="Henry Heffernan - 2022"
-                currentSong={currentSong}
-                setCurrentSong={setCurrentSong}
-            />
+            <h2>Learning Resources</h2>
+            <ul>
+                <li><a href="https://docs.google.com/document/d/1emwVBo-zgxbtzBD7vyQBjHvQnoXnNa7qHD-NlDitsjU/edit">Professional Development with Dr. Dunham</a></li>
+                <li><a href="https://foregoing-surfboard-ae6.notion.site/Intro-to-R-programming-e32d3cb749de410f8240fa3ac4a60f8a">Intro to programming in R</a> from Aaron Baker</li>
+                <li><a href="https://probmods.org/">ProbMods</a> - Probabilistic Models of Cognition</li>
+            </ul>
 
-            <br />
-            <br />
-            <div className="captioned-image">
-                <img src={houseProject} alt="" />
-                <p>
-                    <sub>
-                        <b>Figure 1:</b> A screenshot of the project file of
-                        Timeless. 64 tracks in total.
-                    </sub>
-                </p>
-            </div>
-            <p>
-                I love so much about this track. I'm particularly very proud of
-                the composition and the trichotomy of the happy upbeat energy in
-                the intro and first drop, into the rich and euphoric bridge,
-                before delving into the darkness of the final build and drop. I
-                had an absolute blast making this back in the fall of 2020, and
-                every time I revisit it to tweak it just a little more, I have
-                so much fun.
-            </p>
-            <br />
-            <p>
-                After making this track around fall 2020, my interest for house
-                overall was tapering off. Around this time I was starting to go
-                to the gym more consistently and ended up listening to a lot
-                more hard bass music, specifically drum & bass, mid tempo and of
-                course dubstep.
-            </p>
-            <br />
-            <h2>Mid-Tempo?</h2>
-            <br />
-            <p>
-                Only about a month after taking a step away from house I started
-                working on a mid-tempo track which I called edge. I did a lot of
-                experimenting with this track, and to this day I'm very proud
-                with how it turned out. I never got around to finishing it up
-                and giving it a proper outro, but I'm still very happy with it's
-                current state.
-            </p>
-            <br />
-            <MusicPlayer
-                src={edge}
-                title="Edge [W.I.P.]"
-                subtitle="Henry Heffernan - 2021"
-                currentSong={currentSong}
-                setCurrentSong={setCurrentSong}
-            />
-            <br />
-            <p>
-                This track is so dark and I love it! The bass and percussion are
-                the obvious stars of the piece, and I had a blast designing each
-                of them. The bass in this song is layered with a lot of textures
-                and they really give it a lot of the character it needs to be
-                the driving force of the track. The percussion on the other hand
-                gives the track a lot of depth in the high end so when
-                juxtaposed with the bass it radiates this dark and mysterious
-                vibe.
-            </p>
-            <br />
-            <h2>My love for Drum & Bass</h2>
-            <br />
-            <p>
-                I've always loved drum & bass, and over the years I feel like I
-                always come back to it. My first song I ever made was a DnB
-                track and as I've continued to make music over these last few
-                years the way I produce DnB really reflects my specific taste at
-                that time. It's very fun looking back at the various DnB styles
-                I've taken stabs at over time and how my taste has evolved.
-            </p>
-            <br />
-            <p>
-                The track below is a snip-it from a DnB track I started work on
-                back in 2019. I recently decided to pick it up again and work on
-                it more because I loved the idea, bassline and especially the
-                drums. Below is a short demo of the track including a small
-                buildup and drop.
-            </p>
-            <br />
-            <MusicPlayer
-                src={dnb}
-                title="Break [Demo]"
-                subtitle="Henry Heffernan - 2022"
-                currentSong={currentSong}
-                setCurrentSong={setCurrentSong}
-            />
-            <br />
-            <p>
-                Like I said, I decided to come back to this track because I
-                absolutely loved so much about it. When I picked up work on it
-                again I decided to totally remix the drums and I'm super proud
-                with how they turned out. So proud in fact, I wanted to show
-                them off some more on their own! Below are the drums in the
-                above track, but isolated. I have also included the sub bass
-                because it is drum and bass after all!
-            </p>
-            <br />
-            <MusicPlayer
-                src={dnbDrums}
-                title="Break [Drums and Sub]"
-                subtitle="Henry Heffernan - 2022"
-                currentSong={currentSong}
-                setCurrentSong={setCurrentSong}
-            />
-            <br />
-            <div className="captioned-image">
-                <img src={dnbDrumsProject} alt="" />
-                <p>
-                    <sub>
-                        <b>Figure 2:</b> Drums and sub for the track Break. See
-                        if you can follow along in the image as the audio above
-                        plays!
-                    </sub>
-                </p>
-            </div>
-            <br />
-            <p>
-                There are a lot of layers to DnB drums, and I honestly find them
-                the hardest to mix and get right since the sound design of DnB
-                so often wants to fight in the same frequency range as the
-                drums. Because of this I use very aggressive sidechain to keep
-                the drums clear while giving me the freedom to not have to
-                overly eq the bass and general sound design. I also find that
-                parallel compression on drums in DnB can be an absolute life
-                saver to keep the drums controlled but still hard hitting.
-            </p>
-            <br />
-            <h2>The Future of my music</h2>
-            <br />
-            <p>
-                I love to make music and will continue to make music as long as
-                I can. I think soon enough I will be releasing music publicly
-                since I feel like my personal style has slowly developed into
-                something I am proud of over these last few years. I also want
-                to one day make an album, but that is a long ways away.
-            </p>
-            <br />
-            <p>
-                I am aware this section of my portfolio is rather short, but
-                it's because I am very selective with what I want to show off to
-                the world. I've been producing for over 6 years and have made
-                hundreds maybe even a thousand tracks/loops in that time. I only
-                recently have been feeling very content with the stuff I've made
-                and I'm very excited to see what I can do for the future. Thanks
-                for listening I hope you enjoyed!
-            </p>
-            <br />
+            <h2>My Portfolio</h2>
+            <ul>
+                <li><a href="https://www.kaggle.com/code/mishaokeeffe/mishamnist">MNIST neural network classifier</a></li>
+                <li><a href="https://drive.google.com/drive/folders/17uDqaQGKFVH_Syx5Er6OvcdSXh34CBTf">Application Materials</a></li>
+                <li><a href="https://drive.google.com/file/d/1WzYFz0r-UctfGudL093PDIL_sNtxwKgX/view">NASA Mission Concept Proposal PDR</a></li>
+            </ul>
+
+            <h2>Career Resources</h2>
+            <ul>
+                <li><a href="https://puddle-wealth-097.notion.site/Post-Bac-b6452ddc092f48b2b85de5fac19ac831?pvs=4">All things post-bacc</a> - A comprehensive guide for post-baccalaureate opportunities</li>
+            </ul>
+
+            <h2>Inspiring Websites</h2>
+            <p>Here are some websites I find inspiring:</p>
+            <ul>
+                <li><a href="https://www.michellewong.page/home">Michelle Wong</a></li>
+                <li><a href="https://www.adaniabutto.com/">Adani Abutto</a></li>
+            </ul>
         </div>
     );
 };
 
-// const styles: StyleSheetCSS = {};
-
-export default MusicProjects;
+export default OpenScienceResources;
