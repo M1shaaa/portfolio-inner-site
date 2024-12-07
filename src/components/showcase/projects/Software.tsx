ff9adabb7b59f082e5693543ffd374d59a38674e
Misha O'Keeffe <meokeeffe@wisc.edu>
2024-12-07 11:41:46 -0800

    adventures

--- src/components/showcase/projects/Software.tsx
+++ src/components/showcase/projects/Software.tsx
@@ -18,34 +18,42 @@ const SoftwareProjects: React.FC<SoftwareProjectsProps> = () => {
             <div className="text-block">
                 <h2>the ironman</h2>
                 <p>
-                    In August 2022, probably two weeks into moving to Madison, I had my very first shift
-                    at the restaurant I was going to be working at. It also happened to be the same day
-                    that the Madison ironman was going on. Because all the roads were shut down, it was 
-                    super hard for us to get orders out to our delivery drivers. It eventually got to the 
-                    point where I decided to run the food out to the drivers myself. While running with 
-                    my uniform on and food in hand, I thought to myself just how cool it would be to run 
-                    the Madison Ironman.
+                    [coming soon!]
                 </p>
                 <br />
+            </div>
+            <div className="text-block">
+                <h2>ultra running</h2>
+                <h4>the 100 miler</h4>
                 <p>
-                    Fast forward to April and I (impulsively) signed up to run the following year. Most of my summer
-                    that year was spent trying to get into good enough shape such that I wouldn't embarass myself too
-                    much when the time came. In all honestly, this was dreadful and I more or less gave up and stopped 
-                    training completely about a month or so out from the race. Nonetheless, I decided to try anyway. The day 
-                    of, I was barely able to make each cutoff time and finished in about 14 or 15 hours. This was probably 
-                    the hardest thing I have ever done--would not recommend lol. 
+                    [coming soon!] 
+                </p>
+                <h4>the 50 miler</h4>
+                <p>
+                    [coming soon!] 
                 </p>
             </div>
             <div className="text-block">
-                <h2>running</h2>
+                <h2>mountain climbing</h2>
+                <h4>nevado de tolima - Colombia (17,100')</h4>
                 <p>
-                    [coming soon!]
+                    [coming soon!] 
                 </p>
-            </div>
-            <div className="text-block">
-                <h2>mountaineering</h2>
+                <h4>mt. shasta (14,179')</h4>
                 <p>
-                    [coming soon!]
+                    [coming soon!] 
+                </p>
+                <h4>mt. whitney (14,505')</h4>
+                <p>
+                    [coming soon!] 
+                </p>
+                <h4>longs peak (14,259′')</h4>
+                <p>
+                    [coming soon!] 
+                </p>
+                <h4>longs peak (14,438′')</h4>
+                <p>
+                    [coming soon!] 
                 </p>
             </div>
         </div>
