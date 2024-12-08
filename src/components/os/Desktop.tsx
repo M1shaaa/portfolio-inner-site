2ba82069e8208ef4deaf0b62c5fc58b0f9365d75
Misha O'Keeffe <meokeeffe@wisc.edu>
2024-12-07 19:11:23 -0800

    6

--- src/components/os/Desktop.tsx
+++ src/components/os/Desktop.tsx
@@ -10,6 +10,7 @@ import Toolbar from './Toolbar';
 import DesktopShortcut, { DesktopShortcutProps } from './DesktopShortcut';
 import Scrabble from '../applications/Scrabble';
 import Photos from '../applications/Photos';
+import ghibliAudio from '../../assets/audio/ghibli.mp3';  // Add this line
 import Music from '../applications/Music';  // Changed the path
 import { IconName } from '../../assets/icons';
 import MsPaint from '../applications/MsPaint';
