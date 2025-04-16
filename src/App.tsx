869bdd7da07e7652a49cde50f15ecb6188841ea1
Misha O'Keeffe <meokeeffe@wisc.edu>
2025-04-15 19:18:09 -0700

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Desktop from './components/os/Desktop';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
     return (
