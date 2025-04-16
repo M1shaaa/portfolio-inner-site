578eb595909f9884a79352468220dca9ee20a323
Misha O'Keeffe <meokeeffe@wisc.edu>
2025-04-15 19:58:47 -0700

    baii

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -1,30 +1,39 @@
-import Script from 'next/script'
+import React, { useEffect } from 'react';
 
-export default function GoogleAnalytics() {
-  const gaId = process.env.NEXT_PUBLIC_GA_ID;
-  
-  if (!gaId) {
-    return null;
-  }
-  
-  return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gaId}');
-          `,
-        }}
-      />
-    </>
-  );
-}
+const GoogleAnalytics = () => {
+  const gaId = process.env.REACT_APP_GA_ID;
+
+  useEffect(() => {
+    if (!gaId) return;
+    
+    // Load Google Analytics
+    const script1 = document.createElement('script');
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+    script1.async = true;
+    document.head.appendChild(script1);
+
+    // Initialize Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', gaId);
+
+    // Clean up
+    return () => {
+      try {
+        if (script1.parentNode) {
+          document.head.removeChild(script1);
+        }
+      } catch (e) {
+        console.error('Error removing Google Analytics script:', e);
+      }
+    };
+  }, [gaId]);
+
+  return null;
+};
+
+export default GoogleAnalytics;
